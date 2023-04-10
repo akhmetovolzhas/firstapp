da8b8fbf87127dba14755ac0593f2794cdf11390
903-21i <903-21i@903-21i.local>
2023-04-10 20:12:39 +0600

    first commit

---
+++ delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegate
+//
+//  Created by 903-21i on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
